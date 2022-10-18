1e785dc0144b96f7a6ebd4577150d7bddc51bb22
Jenniffer Melchiade <jdenisemelch@gmail.com>
2022-10-18 09:08:42 -0400

    removed comments from actionPanel.jsx.

--- src/components/ActionPanel.jsx
+++ src/components/ActionPanel.jsx
@@ -1,35 +1,18 @@
 import React from "react";
 
-/* Need to build a blank cell list when user clicks CLEAR ALL */
 import { buildCellList } from "../utils";
 
-/**
- * The ActionPanel component represents the interface for updating the Grid
- * based on any number of buttons the user might click on.
- */
 const ActionPanel = (props) => {
   const { activeColor, cellList, setCellList } = props;
-  /**
-   * Create constants for activeColor, cellList, and setCellList, reading them from the props
-   */
 
   return (
     <div className="action-panel">
-      {/* 
-      This button needs an onClick function which:
-        - creates a new cell list using buildCellList
-        - passes the new cell list to setCellList
-    */}
+   
       <button onClick={() => {
         const newList = buildCellList()
         setCellList(newList)
       }}>clear all</button>
-      {/* 
-      This button needs an onClick function which:
-        - creates a new cell list using buildCellList
-        - loops over it, setting the color on each cell to activeColor
-        - passes the new cell list to setCellList
-    */}
+    
       <button onClick={() => {
         const newListColor = buildCellList()
         for (let i = 0; i < newListColor.length; i++){
@@ -37,13 +20,7 @@ const ActionPanel = (props) => {
         }
         setCellList(newListColor)
       }}>fill all</button>
-      {/* 
-      This button needs an onClick function which:
-      - creates a new cell list using buildCellList
-      - loops over the original cellList, and for each cell in it:
-      - set the corresponding (by index) new cell to its color (if it has one) OR
-      - set the corresponding (by index) new cell to the activeColor
-    */}
+   
       <button onClick={() => {
         const newListColorClear = buildCellList()
         console.log(newListColorClear, cellList)
